d411f81850694846723e6dce9f977da479ea4778
alexisjcarr <acarr59@gmail.com>
2019-08-17 19:49:15 -0500

    ✅ Runs John's seeds.
     #1

--- routers/ratingsRouter.js
+++ routers/ratingsRouter.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 // TODO: make model for querying ratings db
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+    
+});
 
 module.exports = router;
